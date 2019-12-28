99c453389ea136a809c2812282330a121792a49b
Roshan Lakmal <roshanlakmal@Roshans-MacBook-Air.local>
2019-12-28 21:55:04 +1100

    removed lable style encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./server-element.component.css']
+  // encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
 
